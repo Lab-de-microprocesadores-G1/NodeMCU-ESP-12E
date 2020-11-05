b8f943acb669e0db298b41f955629b5a25fd4c06
ffarall <37149322+ffarall@users.noreply.github.com>
2020-11-04 23:48:11 -0300

    Scan project added

---
+++ Scan/src/main.cpp
@@ -0,0 +1,54 @@
+#include <Arduino.h>
+#include "ESP8266WiFi.h"
+
+#define LED 2
+#define SCAN_PERIOD 5000
+
+long lastMillis = 0;
+long currentMillis = 0;
+int networks = 0;
+
+void setup() 
+{
+  // put your setup code here, to run once:
+
+  pinMode(LED, OUTPUT);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  Serial.println("Set up ready");
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+
+  currentMillis = millis();
+  if (currentMillis - lastMillis > SCAN_PERIOD) 
+  {
+    lastMillis = currentMillis;
+    digitalWrite(LED, LOW);
+    WiFi.scanNetworks(true);
+    Serial.println();
+    Serial.println("Scan start...");
+  }
+
+  networks = WiFi.scanComplete();
+  if (networks >= 0)
+  {
+    digitalWrite(LED, HIGH);
+    Serial.print(networks);
+    Serial.println("Networks found!");
+
+    for (int i=0; i < networks; i++)
+    {
+      Serial.print(i+1);
+      Serial.print(":");
+      Serial.println(WiFi.SSID(i));
+    }
+
+    WiFi.scanDelete();
+  }
+}
